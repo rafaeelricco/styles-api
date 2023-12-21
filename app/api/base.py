f71e160ac500ba4cdefefdf301c764e8c8661e2d
rafaeelricco <rafaelricco10@gmail.com>
2023-12-21 18:07:47 -0300

    fix: added support to relative path

--- app/api/base.py
+++ app/api/base.py
@@ -12,49 +12,32 @@ import json
 api_todos = Blueprint("api_todos", __name__)
 api = Api(api_todos)
 
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.realpath(__file__)))
+
 
 def get_json_file(file_path):
-    if not os.path.exists(file_path):
-        return {"error": "File not found"}
-
-    with open(file_path, "r") as file:
-        return json.load(file)
+    with open(file_path, "r") as f:
+        return json.load(f)
 
 
 class Infratoken(Resource):
     def get(self):
-        styles = get_json_file(
-            "/Users/rafaelricco/Documents/Emana/flask-mongodb/app/styles/colors-infra.json"
-        )
-        images = get_json_file(
-            "/Users/rafaelricco/Documents/Emana/flask-mongodb/app/styles/images-infratoken.json"
-        )
-
-        if "error" in styles or "error" in images:
-            return jsonify({"error": "One or more files not found"}), 404
-
+        styles = get_json_file("app/styles/colors-infra.json")
+        images = get_json_file("app/styles/images-infratoken.json")
         return jsonify({"styles": styles, "images": images})
 
 
 class Dtvm(Resource):
     def get(self):
-        styles = get_json_file(
-            "/Users/rafaelricco/Documents/Emana/flask-mongodb/app/styles/colors-dtvmxpo.json"
-        )
-        images = get_json_file(
-            "/Users/rafaelricco/Documents/Emana/flask-mongodb/app/styles/images-dtvm.json"
-        )
+        styles = get_json_file("app/styles/colors-dtvmxpo.json")
+        images = get_json_file("app/styles/images-dtvm.json")
         return jsonify({"styles": styles, "images": images})
 
 
 class Invista(Resource):
     def get(self):
-        styles = get_json_file(
-            "/Users/rafaelricco/Documents/Emana/flask-mongodb/app/styles/colors-invista.json"
-        )
-        images = get_json_file(
-            "/Users/rafaelricco/Documents/Emana/flask-mongodb/app/styles/images-invista.json"
-        )
+        styles = get_json_file("app/styles/colors-invista.json")
+        images = get_json_file("app/styles/images-invista.json")
         return jsonify({"styles": styles, "images": images})
 
 
